8efb363b0a2a30037d8122bf2dc6e6840c8bda9b
JessicaKuerschner <68183487+JessicaKuerschner@users.noreply.github.com>
2023-01-23 11:29:54 +0100

    Add files via upload

---
+++ javadefjess.js
@@ -0,0 +1,13 @@
+const video = "VideoCure.mp4";
+
+//function functionvideonext() {
+//    if video done playing {
+//        location.href = "alldefs.html";
+//    }
+//}
+
+function functionvideo() {
+    location.href = video;
+}
+
+//if stopped playing, go to next page (with all definitions)
